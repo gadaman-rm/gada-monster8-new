77e52fc620cd55bac56c031bc194b520dcb3915f
mohammad <mohammad.filt@gmail.com>
2025-09-30 11:07:54 +0330

    change STEPPER_ALIGN_XY

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -854,7 +854,7 @@
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
   // Override with 'M422 S<index> X<pos> Y<pos>'
-  #define Z_STEPPER_ALIGN_XY { {  50.00, 50.00 }, { 350.00,  50.00}, { 350.00, 350.00}, { 50.00, 350.00 } }
+  #define Z_STEPPER_ALIGN_XY { {  370.00, 30.00 }, { 30.00,  30.00}, { 370.00, 370.00}, { 30.00, 370.00 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
